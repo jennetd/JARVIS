e7f9e61eb7504380bbd12c051ff19a0f9a2f9498
wamorkart <wamorkar.t@husky.neu.edu>
2019-04-05 10:29:57 -0500

    update

--- runAutoPilot.sh
+++ runAutoPilot.sh
@@ -1,13 +1,12 @@
-UseOTSDAQ=1
 IncludeTelescope=1
-IncludeVME=0
+IncludeVME=1
 IncludeSampic=0
 IncludeDesktopDRS=1
 IncludeTekScope=0
-IncludeKeysightScope=0
+IncludeKeysightScope=1
 SensorConfigurationNumber=1
 DAQConfigurationNumber=1
 
 
-python AutoPilot2.py -io $UseOTSDAQ -it $IncludeTelescope -iv $IncludeVME -is $IncludeSampic -its $IncludeTekScope -iks $IncludeKeysightScope -idt $IncludeDesktopDRS -se $SensorConfigurationNumber -conf $DAQConfigurationNumber
+python AutoPilot.py -it $IncludeTelescope -iv $IncludeVME -is $IncludeSampic -its $IncludeTekScope -iks $IncludeKeysightScope -idt $IncludeDesktopDRS -se $SensorConfigurationNumber -conf $DAQConfigurationNumber
 
