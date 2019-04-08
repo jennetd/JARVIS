2246afe86e4746ba43961c458c3bf267f218e03d
mhussainphys <mhussainphys@uchicago.edu>
2019-04-08 16:41:16 -0500

    add wrapper script to start processing

---
+++ RecoProcesses/StartVMENoTracksProcessing.py
@@ -0,0 +1,18 @@
+from ProcessExec import *
+import ProcessRuns as pr
+
+print "Start VME data processing"
+
+ExecutionOrder = 1 #This is Ascending Run number order
+TimingDAQPID = 3 #3 means VME No Tracks
+SaveWaveForms = True
+ConfigVersion = "v1"
+RunNumber = -1 #-1 means do all runs
+DigitizerKey = 0 #key=0 for VME
+
+#You need to make a file called "key" that sits inside the RecoProcesses directory and contains the key password for the database.
+keyFile = open("key", "r")
+key = str(keyFile.read().strip())
+
+ProcessExec(ExecutionOrder,TimingDAQPID, SaveWaveForms, ConfigVersion,RunNumber,DigitizerKey,key)
+
