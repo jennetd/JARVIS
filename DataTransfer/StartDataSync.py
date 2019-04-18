462e0885fea816dd92dc0f195275598ee1787430
Si Xie <sixie1984@gmail.com>
2019-04-18 12:54:24 -0500

    add script to sync data from timingdaq02 to DELL

---
+++ DataTransfer/StartDataSync.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os
+import datetime
+import time
+import subprocess
+import glob
+import TransferUtils
+
+#You need to change this the kerberos principal being used
+user = "sxie"
+
+#Change to the location disk location
+LocalDataLocation = "/data2/"
+
+#change to name of the testbeam campaign directory
+CampaignDirectoryName = "2019_04_April_CMSTiming"
+
+continueLoop = True
+while continueLoop : 
+
+
+    #Copy All Data from timingdaq02
+    print "\n\n"
+    print "\n##########################################"
+    print   "Synchronization all data from timingdaq02"
+    print   "##########################################\n"
+    command = "rsync -artuv --progress daq@timingdaq02:/home/daq/"+CampaignDirectoryName+"/* " + LocalDataLocation+CampaignDirectoryName+"/"
+    print command
+    os.system(command)
+    time.sleep(0.5)
+    
