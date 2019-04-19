752e2b57a6c7ff8e77108d1013b679c2581959e6
Si Xie <sixie1984@gmail.com>
2019-04-19 10:07:23 -0500

    add feature to check remote filesize and delete+copy again if file size does not match

--- DataTransfer/TransferUtils.py
+++ DataTransfer/TransferUtils.py
@@ -2,32 +2,46 @@ import os
 import glob
 
 def ListXrdRemote(site, directory):
-    tmpRemoteFileList = os.popen("xrdfs root://" + site + " ls " + directory).read().splitlines()    
-    processedRemoteFileList = []
-    for f in tmpRemoteFileList:
-        processedRemoteFileList.append(f.split("/")[-1])
-
-    return processedRemoteFileList
+    command = "xrdfs root://" + site + " ls -l " + directory
+    output = os.popen(command).read().splitlines()    
+    processedRemoteFileDict = {}
+    for f in output:
+        tmp = f.split(" ")
+        filesize=tmp[5]
+        filename=(tmp[6].split("/")[-1])        
+        processedRemoteFileDict[filename] = filesize
+    return processedRemoteFileDict
 
 def ListLocalFiles(directory):
-    localList = []
+    localDict = {}
     tmp = glob.glob(directory+"/*")    
     for f in tmp:
         a = f.split("/")[-1]
-        localList.append(a)
+        size = os.path.getsize(directory+"/"+a)
+        localDict[a]=size
 
-    return localList
+    return localDict
 
 def XrdCopyLocalToRemote(remoteSite, remoteDir, localDir):
 
-    RemoteFileList = ListXrdRemote(remoteSite, remoteDir)
-    LocalFileList = ListLocalFiles(localDir)
+    RemoteFileDict = ListXrdRemote(remoteSite, remoteDir)
+    LocalFileDict = ListLocalFiles(localDir)
     
-    for f in LocalFileList:        
-        if f in RemoteFileList:
+    for f in LocalFileDict:        
+        copyAgain = False
+        if f in RemoteFileDict.keys():
             print f, " : Already present at Remote site " + remoteSite
+            print LocalFileDict[f] , RemoteFileDict[f]
+            if not (int(LocalFileDict[f]) == int(RemoteFileDict[f])):
+                copyAgain = True
+                print "Remote file size does not match. Remove Remote Copy, and Copy Again"
+                command = "xrdfs root://" + remoteSite + " rm " + remoteDir + "/" + f
+                os.system(command)
         else:
-            command = "xrdcp " + localDir+"/" + f + " root://" + remoteSite + "//" + remoteDir + "/"
+            copyAgain = True
+
+        if copyAgain:
+            command = "xrdcp " + localDir+"/" + f + " root://" + remoteSite + "//" + remoteDir + "/"            
             os.system(command)
             
 
