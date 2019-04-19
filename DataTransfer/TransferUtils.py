8705485398ae776c1601dc5f42812648ada99648
Si Xie <sixie1984@gmail.com>
2019-04-19 10:39:08 -0500

    fix a bug

--- DataTransfer/TransferUtils.py
+++ DataTransfer/TransferUtils.py
@@ -28,19 +28,20 @@ def XrdCopyLocalToRemote(remoteSite, remoteDir, localDir):
     LocalFileDict = ListLocalFiles(localDir)
     
     for f in LocalFileDict:        
-        copyAgain = False
+        doCopy = False
         if f in RemoteFileDict.keys():
             print f, " : Already present at Remote site " + remoteSite
             print LocalFileDict[f] , RemoteFileDict[f]
-            if not (int(LocalFileDict[f]) == int(RemoteFileDict[f])):
-                copyAgain = True
+            if not (str(LocalFileDict[f]).strip() == str(RemoteFileDict[f]).strip()):
+                doCopy = True
                 print "Remote file size does not match. Remove Remote Copy, and Copy Again"
                 command = "xrdfs root://" + remoteSite + " rm " + remoteDir + "/" + f
                 os.system(command)
         else:
-            copyAgain = True
+            print f, " : Not present at Remote site " + remoteSite
+            doCopy = True
 
-        if copyAgain:
+        if doCopy:            
             command = "xrdcp " + localDir+"/" + f + " root://" + remoteSite + "//" + remoteDir + "/"            
             os.system(command)
             
