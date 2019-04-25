9ec7de9c484f51e4734713dbc44984294b9fb2bc
Olmo Cerri <olmo.cerri@gmail.com>
2019-04-25 12:17:03 -0500

    Mail sending added

--- Utils/CheckDiskRemainig.py
+++ Utils/CheckDiskRemainig.py
@@ -1,10 +1,12 @@
 import argparse
 import commands
 import time
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument ('-d', '--disk', type=str, default='/dev/sdb4', help='Disk or volume to check')
 parser.add_argument ('-l', '--limit', type=str, default='10G', help='Limit disk space')
+parser.add_argument ('--email', type=str, default='si.xie@cern.ch', help='email address')
 args = parser.parse_args()
 
 size_dic = {'P': 1024**5, 'T': 1024**4, 'G':1024**3, 'M':1024**2, 'K': 1024}
@@ -20,7 +22,12 @@ while(True):
 
 	if Nfree <= Nlim:
 		print time.ctime(time.time())
-		print 'Limit memory reached!'
-		print '[WARNING]: Only {} free left on disk {} ({})\n\n'.format(sfree, out[0], out[-1])
-	
+		subject = 'Limit memory reached!'
+		print subject
+		warning = '[WARNING]: Only {} free left on disk {} ({})\n\n'.format(sfree, out[0], out[-1])
+		print warning
+		if args.email():
+			cmd 'echo \"'+warning+'\" | mail -s \"'+subject+'\" ' + args.email
+			os.system(cmd)
+
 	time.sleep(5)
