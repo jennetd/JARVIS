f00bae5db9d227a9b142482b3dc52ea3ab9b5d27
Jennet Dickinson <dickinson.jennet@gmail.com>
2023-06-28 15:38:42 -0500

    given -run 0 or no -run option read run number from file, else take argument

--- AutoPilot/AutoPilot2.py
+++ AutoPilot/AutoPilot2.py
@@ -1,12 +1,14 @@
 #!/bin/env python
-import sys 
+import sys
 sys.path.append('../BackEndProcesses/')
-import ParseFunctions as pf
-import TCP_com as tp  #in-built 5s delay in all of them
-from AllModules import *
-#from muxQuery import *
-#import GetTemp as gt
+
+
+import TCP_com as tp  # in-built 5s delay in all of them
 import json as js
+from AllModules import *
+import ParseFunctions as pf
+# from muxQuery import *
+# import GetTemp as gt
 
 ################################### Important #########################################
 ######## This parameter defines at what time it is safe to start a new run ############
@@ -17,15 +19,24 @@ import json as js
 #######################################################################################
 
 NumSpillsPerRun = 1
-#################################Parsing arguments######################################
+################################# Parsing arguments######################################
 
 parser = argparse.ArgumentParser(description='Information for running the AutoPilot program. /n /n General Instructions: Start OTSDAQ and Configure by hand. If using OTSDAQ make sure the start and stop seconds in the beginning of the program are hard coded correctly. /n Make sure to add sensor and configuration after each controlled access and pass it as an argument to this script. /n/n /n TekScope Specific Instructions: /n Make sure you hard code the dpo_fastframe path. /n If using the OTSDAQ with TekScope make sure the Use_otsdaq boolean is True in dpo_fastframe script. /n Make Sure you pass all four Scope trigger and channel settings. /n /n Other Digitizer Specific Instructions: /n If not running the TekScope make sure that the run file name in TCP_com is correct.')
-parser.add_argument('-nruns', '--maxIterations', type=int,default=999999, help = 'Number of runs to take',required=False)
+parser.add_argument('-nruns', '--maxIterations', type=int,
+                    default=999999, help='Number of runs to take', required=False)
+
+parser.add_argument('-run','--runNumber',type=int,
+                    default=0, help='Run number', required=False)
 
 args = parser.parse_args()
 Debug = False
 IsTelescope = True
 maxRuns = int(args.maxIterations)
+RunNumber = int(args.runNumber)
+
+read_run = True
+if RunNumber == 0:
+    read_run = False
 
 print("Stopping after %i runs." % maxRuns)
 
@@ -34,10 +45,10 @@ not_started = ['Not started']
 
 # Use Status file to tell autopilot when to stop.
 if os.path.exists("AutoPilot.status"):
-	os.remove("AutoPilot.status")
-statusFile = open("AutoPilot.status","w") 
-statusFile.write("START") 
-statusFile.close() 
+    os.remove("AutoPilot.status")
+statusFile = open("AutoPilot.status", "w")
+statusFile.write("START")
+statusFile.close()
 AutoPilotStatus = 1
 
 print("*********************************************************************")
@@ -46,7 +57,7 @@ print("*********************************************************************")
 print("")
 
 if IsTelescope:
-	print("Tracking Telescope Included")
+    print("Tracking Telescope Included")
 print("")
 print("")
 print("*********************************************************************")
@@ -57,63 +68,72 @@ print("")
 iteration = 0
 while (AutoPilotStatus == 1 and iteration < maxRuns):
 
-	if iteration % 5 == 0:
-		if IsTelescope: StartSeconds,StopSeconds = GetStartAndStopSeconds(30, 10) #33,22
-		else: StartSeconds,StopSeconds = GetStartAndStopSeconds(50, 22)
-		print("start seconds", StartSeconds, "stop seconds", StopSeconds)
+    if iteration % 5 == 0:
+        if IsTelescope:
+            StartSeconds, StopSeconds = GetStartAndStopSeconds(30, 10)  # 33,22
+        else:
+            StartSeconds, StopSeconds = GetStartAndStopSeconds(50, 22)
+        print("start seconds", StartSeconds, "stop seconds", StopSeconds)
 
-	##sync local run number file with ftbf-daq-06
-	tp.GetRunFile()
-	time.sleep(5)
-	RunNumber = tp.GetRunNumber() 
-	print("Next Run %i " % (RunNumber))
-	print("")
+    # sync local run number file with ftbf-daq-06
+    tp.GetRunFile()
+    time.sleep(5)
 
-	############ Wait for safe time to start run ##########
-	wait_until(StartSeconds)
+    if read_run:
+        RunNumber = tp.GetRunNumber()
 
-	### Preparing to start run
+    print("Next Run %i " % (RunNumber))
+    print("")
 
-	tp.UpdateRunNumber(RunNumber+1)
-	tp.SendRunFile() 
+    ############ Wait for safe time to start run ##########
+    wait_until(StartSeconds)
 
-	################### Starting the run ###################
-	StartTime = datetime.now()  
-	print("\nRun %i started at %s" % (RunNumber,StartTime))
-	print("RunNumber",RunNumber)
+    # Preparing to start run
 
-	#Start the otsdaq run here (scopes already started)
-	if not Debug and IsTelescope: tp.start_ots(RunNumber,False)
-	
-	## Minimum run duration
-	time.sleep(60*NumSpillsPerRun)
+    if read_run:
+        tp.UpdateRunNumber(RunNumber+1)
+        tp.SendRunFile()
 
-	### Don't stop run until scope has acquired all events (OK if still writing events disk, though)
-	tclock_finished=time.time()
+    ################### Starting the run ###################
+    StartTime = datetime.now()
+    print("\nRun %i started at %s" % (RunNumber, StartTime))
+    print("RunNumber", RunNumber)
 
-	if not Debug and IsTelescope: tp.stop_ots(False)
+    # Start the otsdaq run here (scopes already started)
+    if not Debug and IsTelescope:
+        tp.start_ots(RunNumber, False)
 
-	StopTime = datetime.now()
-	print("\nRun %i stopped at %s" % (RunNumber,StopTime))
-	print("")
-	print("*********************************************************************")
-	time.sleep(10) #print("")
+    # Minimum run duration
+    time.sleep(60*NumSpillsPerRun)
 
-	Duration = int((StopTime - StartTime).total_seconds())
+    # Don't stop run until scope has acquired all events (OK if still writing events disk, though)
+    tclock_finished = time.time()
 
-	if pf.QueryGreenSignal(True): 		
+    if not Debug and IsTelescope:
+        tp.stop_ots(False)
 
-		SpillTime = (StartTime+timedelta(0,27)).strftime("%Y-%m-%d %H:%M:%S")
-		
-		ETLTimestamp = (datetime.now() - datetime.strptime("2000-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")).total_seconds() #- 3600 ### For daylight saving time 
+    StopTime = datetime.now()
+    print("\nRun %i stopped at %s" % (RunNumber, StopTime))
+    print("")
+    print("*********************************************************************")
+    time.sleep(10)  # print("")
 
-		#################################################
-		#Check for Stop signal in AutoPilot.status file
-		#################################################
-		tmpStatusFile = open("AutoPilot.status","r") 
-		tmpString = (tmpStatusFile.read().split())[0]
-		if (tmpString == "STOP" or tmpString == "stop"):
-			print("Detected stop signal.\nStopping AutoPilot...\n\n")
-			AutoPilotStatus = 0
-		tmpStatusFile.close()
-	iteration += 1
+    Duration = int((StopTime - StartTime).total_seconds())
+
+    if pf.QueryGreenSignal(True):
+
+        SpillTime = (StartTime+timedelta(0, 27)).strftime("%Y-%m-%d %H:%M:%S")
+
+        ETLTimestamp = (datetime.now() - datetime.strptime("2000-01-01 00:00:00",
+                        "%Y-%m-%d %H:%M:%S")).total_seconds()  # - 3600 ### For daylight saving time
+
+        #################################################
+        # Check for Stop signal in AutoPilot.status file
+        #################################################
+        tmpStatusFile = open("AutoPilot.status", "r")
+        tmpString = (tmpStatusFile.read().split())[0]
+        if (tmpString == "STOP" or tmpString == "stop"):
+            print("Detected stop signal.\nStopping AutoPilot...\n\n")
+            AutoPilotStatus = 0
+        tmpStatusFile.close()
+    iteration += 1
